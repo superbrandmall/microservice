4c78e92dfda9168063f131a8653d6ef28646f223
bbjohn <295322187@qq.com>
2018-01-04 16:41:00 +0800

    init

--- template-client/src/main/java/com/sbm/module/templateclient/api/use/domain/Use.java
+++ template-client/src/main/java/com/sbm/module/templateclient/api/use/domain/Use.java
@@ -1,23 +1,18 @@
 package com.sbm.module.templateclient.api.use.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class Use {
 
-	/**
-	 * 模板名称
-	 */
+	@ApiModelProperty(value="模板名称")
 	private String name;
 
-	/**
-	 * 模型
-	 */
+	@ApiModelProperty(value="模型")
 	private Object model;
 
-	/**
-	 * 结果
-	 */
+	@ApiModelProperty(value="结果")
 	private String result;
 
 }
--- template/src/main/java/com/sbm/module/template/api/use/controller/UseController.java
+++ template/src/main/java/com/sbm/module/template/api/use/controller/UseController.java
@@ -4,17 +4,19 @@ import com.sbm.module.common.controller.BaseController;
 import com.sbm.module.common.domain.JsonContainer;
 import com.sbm.module.template.api.use.biz.IUseService;
 import com.sbm.module.templateclient.api.use.domain.Use;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/template/api/use")
+@RequestMapping("/api/use")
 public class UseController extends BaseController{
 
 	@Autowired
 	private IUseService useService;
 
-	@RequestMapping("/processTemplateIntoString")
+	@ApiOperation(value="使用template相关服务", notes="根据use信息返回结果")
+	@RequestMapping(value = "/processTemplateIntoString", method = RequestMethod.POST)
 	@ResponseBody
 	public JsonContainer<Use> processTemplateIntoString(@RequestBody Use vo) {
 		useService.processTemplateIntoString(vo);
