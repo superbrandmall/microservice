973aff219bb8433d4633070ac247d2da0a828e0f
bbjohn <295322187@qq.com>
2018-01-04 09:57:39 +0800

    init

--- mail/src/test/java/com/sbm/module/mail/send/test/SendTest.java
+++ mail/src/test/java/com/sbm/module/mail/send/test/SendTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -51,8 +52,13 @@ public class SendTest {
 		System.out.println(JSON.toJSONString(result));
 
 		String message = "Test message content";
+
 		//when
 		//service.send(recipient, message);
+
+
+
+
 	}
 
 }
