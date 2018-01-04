37dcc0dc066fd7abd733469b7c0b436d5b69ea44
bbjohn <295322187@qq.com>
2018-01-04 15:10:36 +0800

    init

--- template-client/src/main/java/com/sbm/module/templateclient/api/use/biz/IUseClient.java
+++ template-client/src/main/java/com/sbm/module/templateclient/api/use/biz/IUseClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@FeignClient(value = "TEMPLATE")
+@FeignClient(value = "template")
 @RequestMapping("/template/api/use")
 public interface IUseClient {
 
