2a171c810bedfff6ac0961957ca165f2237560ce
bbjohn <295322187@qq.com>
2018-01-04 16:13:04 +0800

    init

--- common/src/main/java/com/sbm/module/common/configuration/CommonConfiguration.java
+++ common/src/main/java/com/sbm/module/common/configuration/CommonConfiguration.java
@@ -6,8 +6,17 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class CommonConfiguration {
 
 	@Autowired
@@ -18,5 +27,28 @@ public class CommonConfiguration {
 	public RestTemplate getRestTepmlate(){
 		return builder.build();
 	}
+
+	/****************************************************************************/
+	// apidocs
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.sbm.module"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("microservice")
+				.description("superbrandmall的微服务")
+				.contact(new Contact("张骏恺", "https://github.com/superbrandmall/microservice" ,"junkai.zhang@superbrandmall.com"))
+				.version("1.0")
+				.build();
+	}
+
 
 }
