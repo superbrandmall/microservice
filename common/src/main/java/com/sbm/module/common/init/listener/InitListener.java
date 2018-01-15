63c25a6a1957daaa436a92e0b035ae41b44a9520
bbjohn <295322187@qq.com>
2018-01-15 18:06:34 +0800

    init

--- common/src/main/java/com/sbm/module/common/init/listener/InitListener.java
+++ common/src/main/java/com/sbm/module/common/init/listener/InitListener.java
@@ -2,6 +2,7 @@ package com.sbm.module.common.init.listener;
 
 import com.sbm.module.common.init.InitAfterLoad;
 import com.sbm.module.common.provider.SpringContextProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,8 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class InitListener {
-
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private SpringContextProvider provider;
@@ -25,19 +25,19 @@ public class InitListener {
 	}
 
 	private void init() {
-		logger.info("InitListener: initialization started");
+		log.info("InitListener: initialization started");
 		long startTime = System.currentTimeMillis();
 		Map<String, InitAfterLoad> map = provider.getBeans(InitAfterLoad.class);
 		for (String key : map.keySet()) {
-			logger.info(key + ": initialization started");
+			log.info(key + ": initialization started");
 			long startTimeTmp = System.currentTimeMillis();
 			// 执行
 			map.get(key).init();
 			long elapsedTimeTmp = System.currentTimeMillis() - startTimeTmp;
-			logger.info(key + ": initialization completed in " + elapsedTimeTmp + " ms");
+			log.info(key + ": initialization completed in " + elapsedTimeTmp + " ms");
 		}
 		long elapsedTime = System.currentTimeMillis() - startTime;
-		logger.info("InitListener: total initialization completed in " + elapsedTime + " ms");
+		log.info("InitListener: total initialization completed in " + elapsedTime + " ms");
 	}
 
 }
