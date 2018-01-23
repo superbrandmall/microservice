ac835626a26b1a0510a54e127df715b3d0cba7b3
bbjohn <295322187@qq.com>
2018-01-23 16:36:58 +0800

    添加cors

--- zuul/src/main/java/com/sbm/module/ZuulApplication.java
+++ zuul/src/main/java/com/sbm/module/ZuulApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,4 +18,18 @@ public class ZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
+
+	@Bean
+	@Order(Integer.MAX_VALUE)
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("*", config);
+		return new CorsFilter(source);
+	}
+
 }
--- zuul/src/main/java/com/sbm/module/zuul/filter/ApiInteractiveFilter.java
+++ zuul/src/main/java/com/sbm/module/zuul/filter/ApiInteractiveFilter.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Component
 @Slf4j
@@ -31,7 +32,14 @@ public class ApiInteractiveFilter extends ZuulFilter {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
 
-		log.info("send {} request to {}, Authorization: {}", request.getMethod(), request.getRequestURL().toString(), request.getHeader("Authorization"));
+		//log.info("send {} request to {}, Authorization: {}", request.getMethod(), request.getRequestURL().toString(), request.getHeader("Authorization"));
+
+		HttpServletResponse response = ctx.getResponse();
+		//log.info("response: {}", response.getHeaderNames());
+
+		for (String name : response.getHeaderNames()) {
+			//log.info("{}: {}", name, response.getHeader(name));
+		}
 		return null;
 	}
 }
