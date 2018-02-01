1db9d8c09b491dc153f882b0a06c7e0585500521
bbjohn <295322187@qq.com>
2018-02-01 16:20:31 +0800

    初始化sync-hd

---
+++ sync-hd/src/main/java/com/sbm/module/SyncHdApplication.java
@@ -0,0 +1,12 @@
+package com.sbm.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncHdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncHdApplication.class, args);
+	}
+}
