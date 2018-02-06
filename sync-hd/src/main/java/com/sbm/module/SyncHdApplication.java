b6e173e6defafc147e047332903fde46690023c1
bbjohn <295322187@qq.com>
2018-02-06 14:34:14 +0800

    sync-hd启用feign和scheduling

--- sync-hd/src/main/java/com/sbm/module/SyncHdApplication.java
+++ sync-hd/src/main/java/com/sbm/module/SyncHdApplication.java
@@ -2,8 +2,14 @@ package com.sbm.module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableScheduling
+@EnableFeignClients
 public class SyncHdApplication {
 
 	public static void main(String[] args) {
--- sync-hd/src/main/java/com/sbm/module/sync/hd/api/modality/biz/impl/ModalityServiceImpl.java
+++ sync-hd/src/main/java/com/sbm/module/sync/hd/api/modality/biz/impl/ModalityServiceImpl.java
@@ -1,27 +1,34 @@
 package com.sbm.module.sync.hd.api.modality.biz.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.netflix.discovery.converters.Auto;
 import com.sbm.module.common.biz.impl.CommonServiceImpl;
+import com.sbm.module.common.domain.JsonContainer;
 import com.sbm.module.common.redis.biz.IRedisService;
+import com.sbm.module.partner.hd.api.biztype.client.IHdBiztypeClient;
+import com.sbm.module.partner.hd.api.biztype.domain.HdBiztype;
 import com.sbm.module.sync.hd.api.modality.biz.IModalityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ModalityServiceImpl extends CommonServiceImpl implements IModalityService {
 
 	@Autowired
 	private IRedisService redisService;
-
-
+	@Autowired
+	private IHdBiztypeClient hdBiztypeClient;
 
 	private static final String PREFIX = "MODALITY_";
 
 	@Override
 	@Scheduled(cron = "0 0 0 * * ?")
 	public void refresh() {
-		System.out.println("1231231231231123123");
-
+		JsonContainer<List<HdBiztype>> result = hdBiztypeClient.findAllVo();
+		System.out.println(JSON.toJSONString(result));
 	}
 
 }
