35896623eee166db076e0c80113528546d08bcd0
bbjohn <295322187@qq.com>
2018-02-08 17:20:53 +0800

    添加baseinfo同步，添加定时任务

---
+++ sync-hd/src/main/java/com/sbm/module/sync/hd/api/baseinfo/biz/impl/BaseInfoServiceImpl.java
@@ -0,0 +1,28 @@
+package com.sbm.module.sync.hd.api.baseinfo.biz.impl;
+
+import com.sbm.module.sync.hd.api.baseinfo.biz.IBaseInfoService;
+import com.sbm.module.sync.hd.api.baseinfo.biz.IBuildingService;
+import com.sbm.module.sync.hd.api.baseinfo.biz.IFloorService;
+import com.sbm.module.sync.hd.api.baseinfo.biz.IMallService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BaseInfoServiceImpl implements IBaseInfoService {
+
+	@Autowired
+	private IMallService mallService;
+	@Autowired
+	private IBuildingService buildingService;
+	@Autowired
+	private IFloorService floorService;
+
+	@Override
+	@Scheduled(cron = "0 0 0 * * ?")
+	public void refresh() {
+		mallService.refresh();
+		buildingService.refresh();
+		floorService.refresh();
+	}
+}
---
+++ sync-hd/src/main/java/com/sbm/module/sync/hd/api/baseinfo/controller/BaseInfoController.java
@@ -0,0 +1,28 @@
+package com.sbm.module.sync.hd.api.baseinfo.controller;
+
+import com.sbm.module.common.annotation.CreateApiDocs;
+import com.sbm.module.common.controller.BaseController;
+import com.sbm.module.common.domain.JsonContainer;
+import com.sbm.module.sync.hd.api.baseinfo.biz.IBaseInfoService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CreateApiDocs
+@RestController
+@RequestMapping("/api/baseinfo")
+public class BaseInfoController extends BaseController {
+
+	@Autowired
+	private IBaseInfoService service;
+
+	@ApiOperation(value = "刷新基础数据数据", notes = "刷新基础数据，包括mall,building,floor")
+	@RequestMapping(value = "/refresh", method = RequestMethod.POST)
+	public JsonContainer refresh() {
+		service.refresh();
+		return setSuccessMessage();
+	}
+
+}
