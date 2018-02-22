2f261cc0c98668d473429faffe1691108fad1582
bbjohn <295322187@qq.com>
2018-02-22 17:48:07 +0800

    添加注释

--- common/src/main/java/com/sbm/module/common/biz/impl/SyncServiceImpl.java
+++ common/src/main/java/com/sbm/module/common/biz/impl/SyncServiceImpl.java
@@ -10,6 +10,11 @@ public abstract class SyncServiceImpl<T, K, F> extends BusinessServiceImpl<T, K>
 
 	private static final String SYNC_INFO = "{} sync, page: {}, pageSize: {}, pageCount: {}, RecordCount: {}";
 
+	/**
+	 * 执行
+	 *
+	 * @param filter
+	 */
 	protected void execute(F filter) {
 		SyncResult<K> result;
 		List<T> pos;
@@ -22,12 +27,35 @@ public abstract class SyncServiceImpl<T, K, F> extends BusinessServiceImpl<T, K>
 		} while (whileCondition(filter, result));
 	}
 
+	/**
+	 * 保存
+	 *
+	 * @param pos
+	 */
 	protected abstract void save(List<T> pos);
 
+	/**
+	 * 获取返回结果
+	 *
+	 * @param filter
+	 * @return
+	 */
 	protected abstract SyncResult<K> getResult(F filter);
 
+	/**
+	 * 后续操作
+	 *
+	 * @param filter
+	 */
 	protected abstract void doAfter(F filter);
 
+	/**
+	 * 判断条件
+	 *
+	 * @param filter
+	 * @param result
+	 * @return
+	 */
 	protected abstract boolean whileCondition(F filter, SyncResult<K> result);
 
 }
