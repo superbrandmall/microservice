82a5d9b701e0db126679cd7540d078df08e3e40b
bbjohn <295322187@qq.com>
2018-02-23 17:14:45 +0800

    修正bug

--- onlineleasing-base/src/main/java/com/sbm/module/onlineleasing/base/fileuploaddetail/repository/ITOLFileUploadDetailRepository.java
+++ onlineleasing-base/src/main/java/com/sbm/module/onlineleasing/base/fileuploaddetail/repository/ITOLFileUploadDetailRepository.java
@@ -1,11 +1,11 @@
 package com.sbm.module.onlineleasing.base.fileuploaddetail.repository;
 
+import com.sbm.module.common.data.repository.IDataRepository;
 import com.sbm.module.onlineleasing.base.fileuploaddetail.domain.TOLFileUploadDetail;
-import com.sbm.module.onlineleasing.data.repository.IOLDataRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "fileuploaddetail")
-public interface ITOLFileUploadDetailRepository extends IOLDataRepository<TOLFileUploadDetail, Integer> {
+public interface ITOLFileUploadDetailRepository extends IDataRepository<TOLFileUploadDetail, Integer> {
 
 	TOLFileUploadDetail findOneByUri(String uri);
 
