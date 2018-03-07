1a4c6d1a1be92ad7f7c1c0edab3f3b1414f6a193
bbjohn <295322187@qq.com>
2018-03-07 14:40:33 +0800

    初始化common-authorization

---
+++ common-authorization/src/main/java/com/sbm/module/CommonAuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.sbm.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonAuthorizationApplication.class, args);
+	}
+}
