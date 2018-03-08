c9605525b0a5f1d35329b6575a2fed6206555256
bbjohn <295322187@qq.com>
2018-03-08 15:06:35 +0800

    添加@EnableFeignClients

--- sync-bi/src/main/java/com/sbm/module/SyncBiApplication.java
+++ sync-bi/src/main/java/com/sbm/module/SyncBiApplication.java
@@ -3,11 +3,13 @@ package com.sbm.module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableScheduling
+@EnableFeignClients
 public class SyncBiApplication {
 
 	public static void main(String[] args) {
