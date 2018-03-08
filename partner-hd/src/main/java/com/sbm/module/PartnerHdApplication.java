cbdf0f7f24d8807d8f481e52ba3c7d68d3581e48
bbjohn <295322187@qq.com>
2018-03-08 11:48:57 +0800

    启用feignClient

--- partner-hd/src/main/java/com/sbm/module/PartnerHdApplication.java
+++ partner-hd/src/main/java/com/sbm/module/PartnerHdApplication.java
@@ -3,9 +3,11 @@ package com.sbm.module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class PartnerHdApplication {
 
 	public static void main(String[] args) {
