f67b592e364488f65e9a31e6e9312e8bd50ccb58
bbjohn <295322187@qq.com>
2018-03-09 13:50:06 +0800

    使用micro_service数据库

--- zuul/src/main/java/com/sbm/module/zuul/filter/ApiInteractiveFilter.java
+++ zuul/src/main/java/com/sbm/module/zuul/filter/ApiInteractiveFilter.java
@@ -4,6 +4,7 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,14 +33,24 @@ public class ApiInteractiveFilter extends ZuulFilter {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
 
-		//log.info("send {} request to {}, Authorization: {}", request.getMethod(), request.getRequestURL().toString(), request.getHeader("Authorization"));
+		log.info("send {} request to {}, Authorization: {}", request.getMethod(), request.getRequestURL().toString(), request.getHeader("Authorization"));
 
 		HttpServletResponse response = ctx.getResponse();
-		//log.info("response: {}", response.getHeaderNames());
+		log.info("response: {}", response.getHeaderNames());
 
 		for (String name : response.getHeaderNames()) {
-			//log.info("{}: {}", name, response.getHeader(name));
+			log.info("{}: {}", name, response.getHeader(name));
 		}
+
+		AntPathMatcher matcher = new AntPathMatcher();
+		System.out.println(request.getContextPath());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getServletPath());
+
+		String method = "/onlineleasing-customer/api/base/info/floor/{mallCode}/{description}";
+
+		System.out.println(matcher.match(method, request.getRequestURI()));
+
 
 		return null;
 	}
