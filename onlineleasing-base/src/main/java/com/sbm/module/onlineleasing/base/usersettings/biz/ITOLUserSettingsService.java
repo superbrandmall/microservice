9822df715528403113083d275a42c05fd59a9fbc
bbjohn <295322187@qq.com>
2018-03-16 15:47:19 +0800

    将onlineleasing 库中的userSettings移除

--- onlineleasing-base/src/main/java/com/sbm/module/onlineleasing/base/usersettings/biz/ITOLUserSettingsService.java
+++
@@ -1,8 +0,0 @@
-package com.sbm.module.onlineleasing.base.usersettings.biz;
-
-import com.sbm.module.onlineleasing.base.usersettings.domain.TOLUserSettings;
-import com.sbm.module.onlineleasing.data.biz.IOLDataService;
-
-public interface ITOLUserSettingsService extends IOLDataService<TOLUserSettings, Integer> {
-
-}
