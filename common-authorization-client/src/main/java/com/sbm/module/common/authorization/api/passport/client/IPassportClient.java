945744c5785fa691649bfaf36ae6a94f8ff1b621
bbjohn <295322187@qq.com>
2018-03-16 16:08:21 +0800

    passport 客户端

---
+++ common-authorization-client/src/main/java/com/sbm/module/common/authorization/api/passport/client/IPassportClient.java
@@ -0,0 +1,17 @@
+package com.sbm.module.common.authorization.api.passport.client;
+
+import com.sbm.module.common.authorization.api.user.domain.User;
+import com.sbm.module.common.domain.JsonContainer;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "common-authorization")
+@RequestMapping("/api/passport")
+public interface IPassportClient {
+
+	@RequestMapping(value = "/login")
+	JsonContainer<User> login(@RequestParam @NotBlank String username, @RequestParam @NotBlank String password);
+
+}
