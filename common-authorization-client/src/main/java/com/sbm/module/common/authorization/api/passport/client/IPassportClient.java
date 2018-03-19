881769385e07ab66818b32891a81eb14c9192b2a
bbjohn <295322187@qq.com>
2018-03-19 11:20:52 +0800

    修正client bug

--- common-authorization-client/src/main/java/com/sbm/module/common/authorization/api/passport/client/IPassportClient.java
+++ common-authorization-client/src/main/java/com/sbm/module/common/authorization/api/passport/client/IPassportClient.java
@@ -5,16 +5,17 @@ import com.sbm.module.common.domain.JsonContainer;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "common-authorization")
 @RequestMapping("/api/passport")
 public interface IPassportClient {
 
-	@RequestMapping(value = "/login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	JsonContainer<User> login(@RequestParam("username") @NotBlank String username, @RequestParam("password") @NotBlank String password);
 
-	@RequestMapping(value = "/updateLastLogin")
+	@RequestMapping(value = "/updateLastLogin", method = RequestMethod.POST)
 	JsonContainer updateLastLogin(@RequestParam("code") @NotBlank String code);
 
 }
