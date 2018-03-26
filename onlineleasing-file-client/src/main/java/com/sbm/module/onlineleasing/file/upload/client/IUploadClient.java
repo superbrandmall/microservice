f1acca8befa7282d9eb5009e37a91e53cd23966d
bbjohn <295322187@qq.com>
2018-03-26 15:45:16 +0800

    添加服务器端调用上传文件代码

--- onlineleasing-file-client/src/main/java/com/sbm/module/onlineleasing/file/upload/client/IUploadClient.java
+++ onlineleasing-file-client/src/main/java/com/sbm/module/onlineleasing/file/upload/client/IUploadClient.java
@@ -3,16 +3,19 @@ package com.sbm.module.onlineleasing.file.upload.client;
 import com.sbm.module.common.domain.JsonContainer;
 import com.sbm.module.onlineleasing.file.upload.config.UploadConfig;
 import com.sbm.module.onlineleasing.file.upload.domain.Upload;
+import com.sbm.module.onlineleasing.file.upload.domain.UploadResult;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @FeignClient(value = "onlineleasing-file", configuration = UploadConfig.class)
 @RequestMapping("/api/upload")
 public interface IUploadClient {
 
 	@RequestMapping(value = "/multi", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	JsonContainer<String> saveUploadDetail(Upload upload);
+	JsonContainer<List<UploadResult>> multi(Upload upload);
 
 }
--- onlineleasing-file-client/src/main/java/com/sbm/module/onlineleasing/file/upload/config/UploadConfig.java
+++ onlineleasing-file-client/src/main/java/com/sbm/module/onlineleasing/file/upload/config/UploadConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+//@Configuration
 public class UploadConfig {
 
 	@Bean
