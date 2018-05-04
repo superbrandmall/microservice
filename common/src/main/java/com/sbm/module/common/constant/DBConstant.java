ed3ea1722b835b36aa16ee63296c18370c90c8a5
bbjohn <295322187@qq.com>
2018-05-04 11:38:18 +0800

    添加DBConstant

---
+++ common/src/main/java/com/sbm/module/common/constant/DBConstant.java
@@ -0,0 +1,15 @@
+package com.sbm.module.common.constant;
+
+public class DBConstant {
+
+	/**
+	 * 有效
+	 */
+	public static final Integer STATE_1 = 1;
+
+	/**
+	 * 失效
+	 */
+	public static final Integer STATE_0 = 0;
+
+}
