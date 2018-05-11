7e0facdc2be320813b546db2958678bc840dbc7e
bbjohn <295322187@qq.com>
2018-05-11 17:57:22 +0800

    优化预约邮件发送，短信发送

--- onlineleasing-customer/src/main/java/com/sbm/module/onlineleasing/customer/reservation/biz/impl/ReservationMessageServiceImpl.java
+++ onlineleasing-customer/src/main/java/com/sbm/module/onlineleasing/customer/reservation/biz/impl/ReservationMessageServiceImpl.java
@@ -27,23 +27,38 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 	private static SimpleDateFormat YYYYMMDDHHMMSS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private static SimpleDateFormat YYYYMMDD = new SimpleDateFormat("yyyy-MM-dd");
 
+	/*********************************************/
 	private static final String LEASING_MAIL_SUBJECT = "OnlineLeasing租赁预约信息邮件";
-	@Value("${reservation.leasing.mail.templateCode}")
-	private String reservationLeasingMailTemplateCode;
-	@Value("${reservation.leasing.mail.name}")
-	private String reservationLeasingMailName;
-	@Value("${reservation.leasing.mail.email}")
-	private String reservationLeasingMailEMail;
+	@Value("${reservation.emp.leasing.name}")
+	private String reservationEmpLeasingName;
+
+	@Value("${reservation.emp.leasing.mail.templateCode}")
+	private String reservationEmpLeasingMailTemplateCode;
+	@Value("${reservation.emp.leasing.mail.email}")
+	private String reservationEmpLeasingMailEMail;
+
+	@Value("${reservation.emp.leasing.sms.templateCode}")
+	private String reservationEmpLeasingSMSTemplateCode;
+	@Value("${reservation.emp.leasing.sms.email}")
+	private String reservationEmpLeasingSMSMobile;
+
+	/*********************************************/
 
 	private static final String EVENT_MAIL_SUBJECT = "OnlineLeasing场地预约信息邮件";
-	@Value("${reservation.event.mail.templateCode}")
-	private String reservationEventMailTemplateCode;
-	@Value("${reservation.event.mail.name}")
-	private String reservationEventMailName;
-	@Value("${reservation.event.mail.email}")
-	private String reservationEventMailEMail;
+	@Value("${reservation.emp.event.name}")
+	private String reservationEmpEventName;
 
+	@Value("${reservation.emp.event.mail.templateCode}")
+	private String reservationEmpEventMailTemplateCode;
+	@Value("${reservation.emp.event.mail.email}")
+	private String reservationEmpEventMailEMail;
 
+	@Value("${reservation.emp.event.sms.templateCode}")
+	private String reservationEmpEventSMSTemplateCode;
+	@Value("${reservation.emp.event.sms.email}")
+	private String reservationEmpEventSMSMobile;
+
+	/*********************************************/
 
 	@Value("${reservation.customer.sms.templateCode}")
 	private String reservationCustomerSmsTemplateCode;
@@ -53,13 +68,13 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 		if (null == vo.getShops() || vo.getShops().isEmpty()) {
 			return;
 		}
-		// 发送短信给顾客
 		sendSMS2Customer(vo);
-		sendMail2Emp(vo);
+
+		send2Emp(vo);
 	}
 
 	private void sendSMS2Customer(Reservation<String> vo) {
-		// 发送邮件 TODO 后续再考虑总线
+		// TODO 后续再考虑总线
 		Map<String, Object> model = new HashMap<>();
 		model.put("userName", vo.getUserName());
 
@@ -72,8 +87,8 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 		checkJsonContainer(smsClient.sendByTemplate(new com.sbm.module.common.message.api.sms.domain.SendByTemplate(vo.getMobile(), null, new Date(), reservationCustomerSmsTemplateCode, model)));
 	}
 
-	private void sendMail2Emp(Reservation<String> vo) {
-		// 发送邮件 TODO 后续再考虑总线
+	private void send2Emp(Reservation<String> vo) {
+		// TODO 后续再考虑总线
 		// 租赁
 		List<Map<String, String>> leasingShops = new ArrayList<>();
 		// 场地
@@ -96,15 +111,17 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 
 		// 租赁
 		if (!leasingShops.isEmpty()) {
-			sendMail2Emp(vo, reservationLeasingMailName, leasingShops, reservationLeasingMailEMail, LEASING_MAIL_SUBJECT, reservationLeasingMailTemplateCode);
+			sendMail2Emp(vo, reservationEmpLeasingName, leasingShops, reservationEmpLeasingMailEMail, LEASING_MAIL_SUBJECT, reservationEmpLeasingMailTemplateCode);
+			sendSMS2Emp(vo, reservationEmpLeasingName, leasingShops, reservationEmpLeasingSMSMobile, reservationEmpLeasingSMSTemplateCode);
 		}
 		// 场地
 		if (!eventShops.isEmpty()) {
-			sendMail2Emp(vo, reservationEventMailEMail, leasingShops, reservationEventMailName, EVENT_MAIL_SUBJECT, reservationEventMailTemplateCode);
+			sendMail2Emp(vo, reservationEmpEventName, eventShops, reservationEmpEventMailEMail, EVENT_MAIL_SUBJECT, reservationEmpEventMailTemplateCode);
+			sendSMS2Emp(vo, reservationEmpEventName, eventShops, reservationEmpEventSMSMobile, reservationEmpEventSMSTemplateCode);
 		}
 	}
 
-	private void sendMail2Emp(Reservation<String> vo, String name, List<Map<String, String>> shops, String email, String subject, String templateCode ){
+	private void sendMail2Emp(Reservation<String> vo, String name, List<Map<String, String>> shops, String email, String subject, String templateCode) {
 		Map<String, Object> model = new HashMap<>();
 		model.put("name", name);
 		model.put("userName", vo.getUserName());
@@ -117,6 +134,18 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 		model.put("rentalLength", vo.getRentalLength());
 		model.put("startDate", YYYYMMDD.format(vo.getStartDate()));
 		checkJsonContainer(mailClient.sendByTemplate(new com.sbm.module.common.message.api.mail.domain.SendByTemplate(email, subject, null, new Date(), templateCode, model)));
+	}
+
+	private void sendSMS2Emp(Reservation<String> vo, String name, List<Map<String, String>> shops, String mobile, String templateCode) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("name", name);
+		model.put("userName", vo.getUserName());
+		model.put("mallName", shops.get(0).get("mallName"));
+		model.put("floorName", shops.get(0).get("floorName"));
+		model.put("unit", shops.get(0).get("unit"));
+		model.put("count", vo.getShops().size());
+		model.put("mobile", vo.getMobile());
+		checkJsonContainer(smsClient.sendByTemplate(new com.sbm.module.common.message.api.sms.domain.SendByTemplate(mobile, null, new Date(), templateCode, model)));
 	}
 
 }
