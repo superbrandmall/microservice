f6362a35f4b9212cd288c57029e7ffa7849fe02f
bbjohn <295322187@qq.com>
2018-05-11 14:57:40 +0800

    优化预约邮件发送

--- onlineleasing-customer/src/main/java/com/sbm/module/onlineleasing/customer/reservation/biz/impl/ReservationMessageServiceImpl.java
+++ onlineleasing-customer/src/main/java/com/sbm/module/onlineleasing/customer/reservation/biz/impl/ReservationMessageServiceImpl.java
@@ -27,13 +27,22 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 	private static SimpleDateFormat YYYYMMDDHHMMSS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private static SimpleDateFormat YYYYMMDD = new SimpleDateFormat("yyyy-MM-dd");
 
-	private static final String MAIL_SUBJECT = "OnlineLeasing租赁预约信息邮件";
+	private static final String LEASING_MAIL_SUBJECT = "OnlineLeasing租赁预约信息邮件";
 	@Value("${reservation.leasing.mail.templateCode}")
 	private String reservationLeasingMailTemplateCode;
-	@Value("${reservation.leasing.mail.leasingName}")
-	private String reservationLeasingMailLeasingName;
-	@Value("${reservation.leasing.mail.leasingMail}")
-	private String reservationLeasingMailLeasingMail;
+	@Value("${reservation.leasing.mail.name}")
+	private String reservationLeasingMailName;
+	@Value("${reservation.leasing.mail.email}")
+	private String reservationLeasingMailEMail;
+
+	private static final String EVENT_MAIL_SUBJECT = "OnlineLeasing场地预约信息邮件";
+	@Value("${reservation.event.mail.templateCode}")
+	private String reservationEventMailTemplateCode;
+	@Value("${reservation.event.mail.name}")
+	private String reservationEventMailName;
+	@Value("${reservation.event.mail.email}")
+	private String reservationEventMailEMail;
+
 
 
 	@Value("${reservation.customer.sms.templateCode}")
@@ -46,7 +55,7 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 		}
 		// 发送短信给顾客
 		sendSMS2Customer(vo);
-		sendMail2Leasing(vo);
+		sendMail2Emp(vo);
 	}
 
 	private void sendSMS2Customer(Reservation<String> vo) {
@@ -63,15 +72,13 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 		checkJsonContainer(smsClient.sendByTemplate(new com.sbm.module.common.message.api.sms.domain.SendByTemplate(vo.getMobile(), null, new Date(), reservationCustomerSmsTemplateCode, model)));
 	}
 
-	private void sendMail2Leasing(Reservation<String> vo) {
+	private void sendMail2Emp(Reservation<String> vo) {
 		// 发送邮件 TODO 后续再考虑总线
-		Map<String, Object> model = new HashMap<>();
-		model.put("leasingName", reservationLeasingMailLeasingName);
-		model.put("userName", vo.getUserName());
+		// 租赁
+		List<Map<String, String>> leasingShops = new ArrayList<>();
+		// 场地
+		List<Map<String, String>> eventShops = new ArrayList<>();
 
-		model.put("count", vo.getShops().size());
-
-		List<Map<String, String>> shops = new ArrayList<>();
 		Map<String, String> shop;
 		for (String shopCode : vo.getShops()) {
 			shop = new HashMap<>();
@@ -79,16 +86,37 @@ public class ReservationMessageServiceImpl extends CommonServiceImpl implements 
 			shop.put("mallName", shopInfo.getMallName());
 			shop.put("floorName", shopInfo.getFloorName());
 			shop.put("unit", shopInfo.getUnit());
-			shops.add(shop);
-		}
-		model.put("shops", shops);
 
+			if ("正柜".equals(shopInfo.getSubType())) {
+				leasingShops.add(shop);
+			} else if ("固定场地".equals(shopInfo.getSubType()) || "公共区域".equals(shopInfo.getSubType()) || "临时场地".equals(shopInfo.getSubType())) {
+				eventShops.add(shop);
+			}
+		}
+
+		// 租赁
+		if (!leasingShops.isEmpty()) {
+			sendMail2Emp(vo, reservationLeasingMailName, leasingShops, reservationLeasingMailEMail, LEASING_MAIL_SUBJECT, reservationLeasingMailTemplateCode);
+		}
+		// 场地
+		if (!eventShops.isEmpty()) {
+			sendMail2Emp(vo, reservationEventMailEMail, leasingShops, reservationEventMailName, EVENT_MAIL_SUBJECT, reservationEventMailTemplateCode);
+		}
+	}
+
+	private void sendMail2Emp(Reservation<String> vo, String name, List<Map<String, String>> shops, String email, String subject, String templateCode ){
+		Map<String, Object> model = new HashMap<>();
+		model.put("name", name);
+		model.put("userName", vo.getUserName());
+		model.put("count", shops.size());
+		model.put("shops", shops);
 		model.put("merchantName", vo.getMerchantName());
 		model.put("mobile", vo.getMobile());
 		model.put("brandName", vo.getBrandName());
 		model.put("reserveTime", YYYYMMDD.format(vo.getReserveTime()));
 		model.put("rentalLength", vo.getRentalLength());
 		model.put("startDate", YYYYMMDD.format(vo.getStartDate()));
-		checkJsonContainer(mailClient.sendByTemplate(new com.sbm.module.common.message.api.mail.domain.SendByTemplate(reservationLeasingMailLeasingMail, MAIL_SUBJECT, null, new Date(), reservationLeasingMailTemplateCode, model)));
+		checkJsonContainer(mailClient.sendByTemplate(new com.sbm.module.common.message.api.mail.domain.SendByTemplate(email, subject, null, new Date(), templateCode, model)));
 	}
+
 }
